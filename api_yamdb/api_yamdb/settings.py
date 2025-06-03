e8e52c9b7a3263ed8f2a6287e96056d54dfb4b42
Софья Болтнева <sofya.boltneva@yandex.ru>
2025-06-03 19:03:20 +0500

    Удалена лишняя строка в settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'rest_framework_simplejwt',
     'django_filters',
     'users',
